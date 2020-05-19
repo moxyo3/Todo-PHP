ff7357684a6ba582da6b01238d98b9876525a30b
moxyo3 <1254momomo@gmail.com>
2020-05-19 09:43:36 +0900

    PHP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TaskSystem-PHP
 タスク管理システム(PHP)
+[ubuntu 18.04 インストール(6) apache, php](http://verifiedby.me/adiary/0122)
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Todoリスト</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>name:</td>
+            <td><input type= "text" id= "name"></td>
+        </tr>
+        <tr>
+            <td>Todo:</td>
+            <td><input type="text" id = "todo"></td>
+        </tr>
+        <tr><button onclick="createTodo()"></tr>
+    </table>
+    
+</body>
+</html>
