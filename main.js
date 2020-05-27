be28e14345e1e3c1409ca0216ad7d9e6abdc1dcf
moxyo3 <1254momomo@gmail.com>
2020-05-28 00:07:45 +0900

    main.php

--- main.js
+++ main.js
@@ -1,33 +1,40 @@
 function createTodo(){
-    alert("js読み込み");
     //fetchでajax通信、json形式でphpにpost
-    const name = document.getElementbyid("name");
-    const todo = document.getElementbyid("todo");
+    const name = document.getElementById("name");
+    const todo = document.getElementById("todo");
 
-    fetch("main.php", {
-        method: "post",
+    const form = {
+        name: name.value,
+        todo: todo.value,
+    }
+
+    console.log(form.name);
+    console.log(form.todo);
+
+    fetch('./main.php', {
+        method: 'POST',
         headers: {
             "content-type": "application/json"
         },
-        body: JSON.stringify({
-            "name": name;
-            "todo": todo;
-        })
-    }).then(response => {
-        return response.json();)
+        body: JSON.stringify(form),
+    }).then((response) => {
+        if (response.ok){
+            alert("登録しました");
+            //getTodo();
+        } else {
+            alert("登録失敗しました");
+        }
+    }).catch((err) => {
+        console.log(err);
     })
-    .then(json=> {
-        console.log(json);
-    })
-    .catch(e => {
-        console.error(e);
-    });
-//成功したらcreatetable呼ぶ
 }
 
-function createtable(){
-    //再読込なしでテーブル再生成
-    //documentを動的に生成
-    const table = document.getelementbyid("table");
-    table.innerhtml = "";
-}
+//function createtable(){
+//    //再読込なしでテーブル再生成
+//    //documentを動的に生成
+//    const table = document.getelementbyid("table");
+//    table.innerhtml = "";
+//}
+
+//function getTodo(){
+//};
--- main.php
+++ main.php
@@ -1,11 +1,26 @@
 <?php
-function createTodo(){
-    $formData = json_decode(file_get_contents('php://input'), true);
-$name = $formData["name"];
-$todo = $formData["todo"];
-echo $name;
-echo $todo;
+//$_POSTにapplication/jsonは渡せないので
+//file_get_contents("php://input")でリクエストボディを取得
+//json_decodeでパースする
+    //POST受取、jsonのデコード
+    $content = file_get_contents("php://input");
+    //第二引数のtrue: 連想配列にする
+    $decoded = json_decode($content, true);
+    $name = $decoded["name"];
+    $todo = $decoded["todo"];
 
-//POST受取、jsonのデコード
-//DB接続処理
-}
+    //DB接続
+    try {
+        //PDOインスタンス生成
+        $pdo = new PDO(
+            "mysql:host=localhost; dbname=test_db; charset=utf8mb4",
+            'moxyooo',
+            'moxyooo',
+        );
+        $stmt = $pdo->prepare('INSERT INTO todos (name, todo) VALUES (:name, :todo);');
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':todo', $todo);
+        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
