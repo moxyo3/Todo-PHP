13e93fd0080f58eb1800ae68c75b853ebfaeb178
moxyo3 <1254momomo@gmail.com>
2020-06-03 21:11:41 +0900

    pdo修正

--- main.php
+++ main.php
@@ -15,15 +15,19 @@ switch ($_SERVER["REQUEST_METHOD"]){
 }
 
 function createTodo(){
-//$_POSTにapplication/jsonは渡せないので
-//file_get_contents("php://input")でリクエストボディを取得
-//POST受取、jsonのデコード
-    $pdo = dbConnect();
+    //$_POSTにapplication/jsonは渡せないので
+    //file_get_contents("php://input")でリクエストボディを取得
+    //POST受取、jsonのデコード
+    global $pdo;
     $content = file_get_contents("php://input");
     //第二引数のtrue: 連想配列にする
     $decoded = json_decode($content, true);
-    $name = $decoded["name"];
-    $todo = $decoded["todo"];
+    if($decoded === null){
+        throw new Exception('JSON Error');
+    } else {
+        $name = $decoded["name"];
+        $todo = $decoded["todo"];
+    }
 
     $sql = 'INSERT INTO todos (name, todo) VALUES (?, ?);';
     $stmt = $pdo->prepare($sql);
@@ -33,17 +37,22 @@ function createTodo(){
 }
 
 function getTodo(){
-    $pdo = dbConnect();
+    global $pdo;
     $sql = 'SELECT * FROM todos';
     $stmt = $pdo->query($sql);
 
     $rows = $stmt-> fetchAll(PDO::FETCH_ASSOC);
     header('Content-type:application/json');
-    echo json_encode($rows,JSON_UNESCAPED_UNICODE);
+    $json = json_encode($rows,JSON_UNESCAPED_UNICODE);
+    if ($json === null) {
+        throw new Exception ('JSON Error');
+    } else {
+        echo $json;
+    }
 }
 
 function deleteTodo(){
-    $pdo = dbConnect();
+    global $pdo;
     $sql= 'DELETE from todos WHERE id = ?';
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(1,$_REQUEST["id"]);
